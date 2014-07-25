252cbf6b229795585d7b7cbc661a8e914e961a61
Austin Jackson themacphage@gmail.com <themacphage@gmail.com>
2014-07-25 18:44:29 -0500

    Revert "First playable build!"
    This reverts commit fe3a29d4d728563bfa97455b1ab65334fa7e045b.

--- src/com/ruthlessphysics/ptictactoe/Player.java
+++ src/com/ruthlessphysics/ptictactoe/Player.java
@@ -59,7 +59,7 @@ public class Player
 			try
 			{
 				box = Integer.parseInt(JOptionPane.showInputDialog(getName()+", what box number should your next move be in?"));
-				if(box == -1)
+				if(box == -3)
 				{
 					Debug.alert("Intentionally quitting.");
 					Display.destroy();
