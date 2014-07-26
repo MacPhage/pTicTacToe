29410520ffa3e9675789714bf47589aa0d0a6dbf
Austin Jackson themacphage@gmail.com <themacphage@gmail.com>
2014-07-25 19:04:24 -0500

    Obligatory

--- src/com/ruthlessphysics/ptictactoe/Player.java
+++ src/com/ruthlessphysics/ptictactoe/Player.java
@@ -59,11 +59,7 @@ public class Player
 			try
 			{
 				box = Integer.parseInt(JOptionPane.showInputDialog(getName()+", what box number should your next move be in?"));
-<<<<<<< HEAD
-				if(box == -1) //Obligatory comment because I'm derp
-=======
-				if(box == -3)
->>>>>>> FETCH_HEAD
+				if(box == -1)
 				{
 					Debug.alert("Intentionally quitting.");
 					Display.destroy();
